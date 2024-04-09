41162b719463a68df2528803dc042399adf53971
marcovirinni@gmail.com <marcovirinni@gmail.com>
2024-04-09 20:11:17 -0300

    logic of login end

--- FRONTEND/FooDelivery/src/app/admin-dashboard/admin-dashboard.component.ts
+++ FRONTEND/FooDelivery/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from '../home/header/header.component';
+import { FooterComponent } from '../home/footer/footer.component';
+import { NavAdminComponent } from '../nav-admin/nav-admin.component';
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,FooterComponent,NavAdminComponent],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
