1cc1b44bfd14620673d514841c3e4dfef23ad3b0
Alex David Frías <adavidfruiz@gmail.com>
2024-04-10 01:56:58 -0500

    add Order

---
+++ BACKEND/app/src/main/java/com/example/app/model/Order.java
@@ -0,0 +1,17 @@
+package com.example.app.model;
+
+import java.math.BigDecimal;
+
+public class Order {
+
+    private Long id;
+
+    private Promotion promotionId;
+
+    private Product productId;
+
+    private Short quantity;
+
+    private BigDecimal totalPrice;
+
+}
