7d95cb1ac1619034fd2f33c9ce185af7a2059d4d
nicoramo2s <nicolasramo2s@hotmail.com>
2024-04-11 11:33:41 -0300

    error imports

--- FRONTEND/FooDelivery/src/app/app.component.ts
+++ FRONTEND/FooDelivery/src/app/app.component.ts
@@ -1,19 +1,24 @@
-import {Component, inject} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
-import { PrimeNGConfig } from "primeng/api";
+import { PrimeNGConfig } from 'primeng/api';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   private primengConfig = inject(PrimeNGConfig);
+  menuOption: string = '';
+
+  onOption(menuOption: string) {
+    this.menuOption = menuOption;
+  }
 
   title = 'FooDelivery';
   constructor(private http: HttpClient) {
