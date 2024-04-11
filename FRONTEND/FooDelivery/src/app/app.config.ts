e3a92e122244185838096b427fca935ffece7739
Chris <cristian.28hedz@gmail.com>
2024-04-11 15:00:49 -0600

    chore: add angular social login library and configure it

--- FRONTEND/FooDelivery/src/app/app.config.ts
+++ FRONTEND/FooDelivery/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
-
+import { GoogleLoginProvider,SocialAuthServiceConfig} from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,7 +12,24 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideHttpClient(withFetch()),
     BrowserAnimationsModule,
-    provideAnimations()
-
+    provideAnimations(),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('695631010178-i917jfbar3gsd627n3ec0pk75daglb1m.apps.googleusercontent.com', {
+              oneTapEnabled: false,
+              prompt: 'consent',
+            }),
+          },
+        ],
+        onError: (err) => {
+          console.error(err);
+        },
+      } as SocialAuthServiceConfig,
+    },
   ]
 };
