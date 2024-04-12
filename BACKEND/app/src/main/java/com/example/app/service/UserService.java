74eb2fa6f6c6c525e17802091c388aa3bbac6e44
Hiram Chavez <virtual.liga@gmail.com>
2024-04-12 12:35:32 -0600

    feat(backend): add delete user by admin endpoint

--- BACKEND/app/src/main/java/com/example/app/service/UserService.java
+++ BACKEND/app/src/main/java/com/example/app/service/UserService.java
@@ -226,4 +226,12 @@ public class UserService {
     public Page<SignedUserDTO> getAllUsersByAdmin(Pageable pageable) {
         return userRepository.findAllByActiveTrue(pageable).map(userMapper::userToSignedUserDTO);
     }
+
+    public void deleteByAdmin(Long id) {
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not found in the database"));
+
+        user.setActive(false);
+    }
 }
