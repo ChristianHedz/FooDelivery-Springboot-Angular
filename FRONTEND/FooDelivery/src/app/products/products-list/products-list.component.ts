a21140e54db6c4cea60c3a6cce570b2031a7216c
nicoramo2s <nicolasramo2s@hotmail.com>
2024-04-17 11:40:29 -0300

    update productlist page

--- FRONTEND/FooDelivery/src/app/products/products-list/products-list.component.ts
+++ FRONTEND/FooDelivery/src/app/products/products-list/products-list.component.ts
@@ -2,18 +2,24 @@ import { Component, inject } from '@angular/core';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../services/product.service';
 import { IProductDTO } from '../../admin-dashboard/interfaces/product.interface';
+import { ModalBuyComponent } from './modal-buy/modal-buy.component';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css',
-  imports: [ProductDetailComponent],
+  imports: [ProductDetailComponent, ModalBuyComponent],
 })
 export class ProductsListComponent {
   private productService = inject(ProductService);
   products: IProductDTO[] = [];
   newProducts: IProductDTO[] = [];
+  public carritoProducts: Map<number, IProductDTO> = new Map();
+  public cantidadDeProductosCarrito: number = 0;
+  public precioTotalProductos: number = 0;
+  public totalFinal: number = 0;
+  priceDelivery: number = 500;
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe({
@@ -28,32 +34,56 @@ export class ProductsListComponent {
   }
 
   mapProducts() {
-    let newProducts = this.products.map((product) => ({
-      ...product,
-      count: 0,
-    }));
-    return newProducts;
+    return this.products.map((product) => ({ ...product, count: 0 }));
   }
 
   increment(productId: number | undefined) {
+    if (productId) {
+      this.newProducts.forEach((product) => {
+        if (product.id === productId && product.count != undefined) {
+          product.count += 1;
+          this.cantidadDeProductosCarrito++;
+          this.precioTotalProductos += product.price;
+          this.carritoProducts.set(product.id, product);
+          this.updateTotal();
+        }
+      });
+    }
+  }
+
+  decrement(productId: number | undefined) {
+    if (productId) {
+      this.newProducts.forEach((product) => {
+        if (product.count != undefined)
+          if (product.id === productId && product.count > 0) {
+            product.count -= 1;
+            this.cantidadDeProductosCarrito--;
+            this.precioTotalProductos -= product.price;
+            if (product.count === 0) {
+              this.carritoProducts.delete(product.id);
+            } else {
+              this.carritoProducts.set(product.id, product);
+            }
+            this.updateTotal();
+          }
+      });
+    }
+  }
+
+  deleteProduct(productId: number | undefined) {
     this.newProducts.forEach((product) => {
-      if (product.count === undefined) {
+      if (product.count != undefined && product.id != undefined)
+      if (product.id === productId && product.count > 0) {
+        this.cantidadDeProductosCarrito -= product.count;
+        this.precioTotalProductos -= product.price * product.count;
         product.count = 0;
-      }
-      if (productId == product.id) {
-        product.count += 1;
+        this.carritoProducts.delete(productId);
+        this.updateTotal();
       }
     });
   }
 
-  decrement(productId: number | undefined) {
-    this.newProducts.forEach((product) => {
-      if (product.count === undefined) {
-        product.count = 0;
-      }
-      if (productId == product.id) {
-        product.count -= 1;
-      }
-    });
+  updateTotal() {
+    this.totalFinal = this.precioTotalProductos + this.priceDelivery;
   }
 }
