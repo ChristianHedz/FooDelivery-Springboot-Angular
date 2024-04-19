5d196958f3a4b78ec6e3ac09c9872c02f2d31175
Alex David Frías <adavidfruiz@gmail.com>
2024-04-19 01:17:45 -0500

    clean

--- BACKEND/app/src/main/java/com/example/app/service/OrderService.java
+++ BACKEND/app/src/main/java/com/example/app/service/OrderService.java
@@ -1,16 +1,12 @@
 package com.example.app.service;
 
-import com.example.app.dto.order.AddProductInOrderDTO;
 import com.example.app.dto.order.OrderDto;
 import com.example.app.dto.order.OrderRequestDTO;
-import com.example.app.model.Order;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OrderService {
 
